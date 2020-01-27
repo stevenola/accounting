2d74a7705469c9611bbab74eea005ce6511c3ee9
Stephen Fingerman <steve@avalonretailconsulting.com>
2020-01-27 08:40:14 -0600

    added client and transaction tables

---
+++ database/migrations/2020_01_27_142536_create_clients_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('street');
+            $table->string('city');
+            $table->string('state');
+            $table->integer('zip');
+            $table->integer('retainer');
+            $table->string('recurring');
+            $table->string('print');
+            $table->string('active');
+            $table->string('in_state');
+            $table->string('consultant');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
---
+++ database/migrations/2020_01_27_142932_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('client_id');
+            $table->string('description1');
+            $table->integer('amount1');
+            $table->string('description2')->nullable();
+            $table->integer('amount2')->nullable();
+            $table->string('notes')->nullable();
+            $table->integer('check_no')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
